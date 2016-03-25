ca45773bdb1eec1c8dba9d6e78cd1b0976a0bac8
Steven Thompson <steven.thompson.ca@gmail.com>
2016-03-25 17:34:10 -0400

    initial commit

--- HiExercise/main.m
+++ HiExercise/main.m
@@ -8,10 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+/*
+ Given a string, return true if the string starts with "hi" and false otherwise.
+ startHi(“hi there") → true
+ startHi(“hi") → true
+ startHi(“hello hi") → false
+ */
+
+BOOL startHi(NSString *input) {
+    // lower the case first
+    NSString *inputLower = [input lowercaseString];
+    // if you didn't have prefix you would have to call subStringWithRange and make a range that is Location 0 and length 2, and then call isEqualToString:@"hi".
+    if ([inputLower hasPrefix:@"hi"]) {
+        return YES;
     }
+    return NO;
+}
+
+int main(int argc, const char * argv[]) {
+    BOOL r1 = startHi(@"hi there");
+    BOOL r2 = startHi(@"hi");
+    BOOL r3 = startHi(@"hello hi");
+    
+    NSLog(@"\"hi there\" starts with \"hi\": %@", r1 ? @"true" : @"false");
+    NSLog(@"\"hi\" starts with \"hi\": %@", r2 ? @"true" : @"false");
+    NSLog(@"\"hello hi\" starts with \"hi\": %@", r3 ? @"true" : @"false");
     return 0;
 }
